f4ecaa1e9d0fa10629076bb7ad1980c6d0d1d2ec
rukys <ruky.sektiawan@gmail.com>
2026-01-23 14:10:24 +0700

    config android and ios for every library

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    screens: {},
+    colors: {
+      primary: '#ff6600',
+      secondary: '#292929',
+      accent: '#ff6600',
+      error: '#ff0000',
+      warning: '#ff9800',
+      info: '#2196f3',
+      success: '#4caf50',
+      white: '#ffffff',
+      black: '#000000',
+      grey: '#d9d9d9',
+      softGrey: '#f9f9f9',
+      darkGrey: '#6e6e6e',
+    },
+    fontFamily: {},
+    extend: {},
+  },
+};
