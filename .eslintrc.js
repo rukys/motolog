bca2bf431ab3cb62060337288ccdc8ba1b82747d
rukys <ruky.sektiawan@gmail.com>
2026-01-23 13:36:35 +0700

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-console': 'warn',
+    // 'object-curly-newline': [
+    //   'error',
+    //   {
+    //     ObjectExpression: {
+    //       minProperties: 6,
+    //       multiline: true,
+    //       consistent: true,
+    //     },
+    //     ObjectPattern: { minProperties: 6, multiline: true, consistent: true },
+    //     ImportDeclaration: 'never',
+    //     ExportDeclaration: {
+    //       minProperties: 6,
+    //       multiline: true,
+    //       consistent: true,
+    //     },
+    //   },
+    // ],
+  },
 };
